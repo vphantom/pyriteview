9cf72c1437ea9c35616333a7c996e68bf295dbf3
Stéphane Lavergne <lis@imars.com>
2016-11-11 10:55:34 -0500

    Use Memcached as session back-end (#1)

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('session.save_handler',   'memcached');
+ini_set('session.gc_maxlifetime', 24 * 60 * 60);
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $twig = new Twig_Environment(

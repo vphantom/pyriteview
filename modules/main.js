2d33081a997a370d18ca4997244e657f413e9c69
Stéphane Lavergne <lis@imars.com>
2017-05-11 11:05:58 -0400

    Copyright consent mandatory checkbox (closes #56)

---
+++ modules/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var $ = global.jQuery = require('jquery');
+
+$().ready(function() {
+  var articleEditForm = $('form#articles_edit');
+  var copyrightLink = $('a#copyright_link');
+
+  /**
+   * Update copyright URL
+   *
+   * The base found in the document in its initial state is appended 't' and
+   * 'aa' CGI variables containing respectively the title and a
+   * semicolon-delimited list of author user IDs.
+   *
+   * @return {void}
+   */
+  function updateCopyright() {
+    copyrightLink.attr(
+        'href',
+        copyrightLink.attr('baseHREF')
+        + '&t='
+        + encodeURIComponent(articleEditForm.find('#title').val())
+        + '&aa='
+        + encodeURIComponent(articleEditForm.find('#authors').val())
+    );
+  }
+
+  if (articleEditForm.length) {
+    copyrightLink.attr('baseHREF', copyrightLink.attr('href'));
+
+    // Let things settle, then initialize copyright URL
+    setTimeout(updateCopyright, 1000);
+
+    // Update copyright URL when relevant fields are modified
+    articleEditForm.find('#title').on('change', updateCopyright);
+    articleEditForm.find('#authors').on('change', updateCopyright);
+  }
+});
